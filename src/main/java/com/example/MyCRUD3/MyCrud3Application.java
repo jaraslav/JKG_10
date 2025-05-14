5b80a9ead55cf56d579ff7a07a339f9807ca7326
jaraslav <Jrikygzz@gmail.com>
2025-05-14 19:45:59 +0300

    ()

---
+++ src/main/java/com/example/MyCRUD3/MyCrud3Application.java
@@ -0,0 +1,13 @@
+package com.example.MyCRUD3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCrud3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCrud3Application.class, args);
+	}
+
+}
