2c0491037d097d5a8e9d5852e64877bd3bbb11c5
jaraslav <Jrikygzz@gmail.com>
2025-05-15 20:55:45 +0300

    ()

--- src/main/java/com/example/MyCRUD3/MyCrud3Application.java
+++ src/main/java/com/example/MyCRUD3/MyCrud3Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyCrud3Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MyCrud3Application.class, args);
 	}
-
 }
